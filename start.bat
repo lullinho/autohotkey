4d8f1b5fc6775fe2fa67192f63ba639130fe28fc
Felipe Lullio <felipelullio@gmail.com>
2019-10-22 05:36:09 -0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+chcp 65001
+
+start chrome.exe "https://www.mercadolivre.com.br/" "https://www.mercadopago.com.br/activities#!/money-requests-sent" "https://web.whatsapp.com/" "https://www.youtube.com/watch?v=jLk5-CohLmE"
+start D:\"Program Files"\Everything\Everything.exe
+start "" "C:\Users\Serviços PS3&PS4\Desktop\platinas 3"
+
+exit
